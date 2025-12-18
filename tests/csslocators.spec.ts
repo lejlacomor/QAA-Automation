ff220c7789ebaccb567c670bf9e2bcea33c2f252
lejlacomor <lejla.comor@htecgroup.com>
2025-12-18 16:13:01 +0100

    Fixed failed assertions

--- tests/csslocators.spec.ts
+++ tests/csslocators.spec.ts
@@ -1,26 +1,28 @@
 import {test, expect, Locator } from "@playwright/test";
 
-test("Verify CSS locators",async({page})=>{
-    await page.goto("https://demowebshop.tricentis.com/");
-    //tag#id
-    // const searchbox: Locator= page.locator("input#small-searchterms");
-    // await searchbox.fill("T-Shirts");
 
-    // expect(page.locator("input#small-searchterms")).toBeVisible();
-    // await page.locator("#small-searchterms").fill("T-Shirt");
+   test("Verify Playwright Locators", async ({ page }) => {
+ 
+  await page.goto("https://demo.nopcommerce.com/");
+ 
+ 
+  await page.route("**/turnstile/**", route => route.abort());
+  await page.route("**cloudflare**", route => route.abort());
+  await page.route("**/challenge**", route => route.abort());
+ 
+   
+ 
+await page.getByRole("link", { name: "Register" }).click();
+await page.waitForTimeout(1000);
+ 
 
+ 
+await page.getByLabel("First name").fill("John");
+await page.waitForTimeout(1000);
+ 
+await page.getByLabel("Last name").fill("Kenedy");
+await page.waitForTimeout(1000);
+ 
+});
     
-    //tag.class
-    //await page.locator("input.search-box-text").fill("T-Shirt");
-//     await page.locator(".search-box-text").fill("T-Shirt");
 
-//tag [atribute=value]
-
-    // await page.locator("[name=''q']").fill("T-Shirts");
-    // await page.waitForTimeout(2000);
-
-    //tag.class[atribute=value]
-    await page.locator("input.search-bpx-text[value='Search store']").fill("T-Shirts");
-    await page.waitForTimeout(2000);
-
-})
